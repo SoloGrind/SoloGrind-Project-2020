b18d2e275952eb2e91085826740546bcecafbd7e
SoloGrind <70855517+SoloGrind@users.noreply.github.com>
2020-09-18 18:40:16 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,116 +5,107 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        BankAccount start = new BankAccount("Angelo Rodriguez", "022301");
-        start.showMenu();
-
-
+        operation menu = new operation();
+        menu.mainMenu();
     }
 }
 
-class BankAccount {
+class operation {
+    int Value;
+    int Subtraction;
+    int previousAmountAdd;
+    char stopAdd = '\0';
 
-    int balance;
-    int previousTransaction;
-    String customerName;
-    String customerId;
-
-    BankAccount(String cname, String cid) {
-        customerName = cname;
-        customerId = cid;
-    }
-
-    void deposit(int amount) {
-
-        if (amount > 0) ;
-        balance = balance + amount;
-        previousTransaction = amount;
-    }
-
-    void withdraw(int amount) {
-        if (amount != 0) ;
-        balance = balance - amount;
-        previousTransaction = -amount;
-    }
-
-    void getPreviousTransaction() {
-        if (previousTransaction > 0) {
-            System.out.println("Deposited: " + previousTransaction);
-        } else if (previousTransaction < 0) {
-            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
-        } else {
-            System.out.println("No transaction occured.");
+    void MethodAddition(int amount) {
+        if (amount > 0) {
+            Value = Value + amount;
+            previousAmountAdd = Value;
         }
     }
+     void MethodSubtraction(int amount) {
+        Value = Value - amount;
+        previousAmountAdd = Value;
+     }
+     void MethodMultiplication(int amount) {
+        Value = Value * amount;
+        previousAmountAdd = Value;
+     }
+     void MethodDivision(int amount) {
+        Value = Value / amount;
+        previousAmountAdd = Value;
+    }
 
-    void showMenu() {
-        char option = '\0';
+      void MethodErase(int amount) {
+        Value = amount;
+        previousAmountAdd = Value;
+
+      }
+
+    void mainMenu() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome " + customerName);
-        System.out.println("Your ID is " + customerId);
-        System.out.println("\n");
-        System.out.println("A. Check Balance");
-        System.out.println("B. Deposit");
-        System.out.println("C. Withdraw");
-        System.out.println("D. PreviousTransaction");
-        System.out.println("E. Exit");
+        char inside = '\0';
+        System.out.println("======================================================================================");
+        System.out.println("================================Welcome=to=CalTextulator==============================");
+        System.out.println("======================================================================================");
+        System.out.println("========================Please=select=a=letter=you=want=to=use========================");
+
+
         do {
-            System.out.print("=========================================================================================================");
-            System.out.print("\n");
-            System.out.println("Enter an option A. [CHECK BALANCE] B. [DEPOSIT] C. [WITHDRAW] D. [PREVIOUS TRANSACTION] E. [EXIT]");
-            System.out.println("=========================================================================================================");
-            option = input.next().charAt(0);
-            System.out.println("\n");
+            System.out.println("======================================================================================");
+            System.out.println("A. Addition B. Subtraction C. Multiplication D. Division F. ERASE NUMBER E. Exit");
+            System.out.println("======================================================================================");
+            inside = input.next().charAt(0);
 
-             switch(option) {
-                 case ('A'):
-                     System.out.println("=========================================================================================================");
-                     System.out.println("Balance = " + balance);
-                     System.out.println("=========================================================================================================");
-                     System.out.println("\n");
-                     break;
-                 case ('B'):
-                     System.out.println("=========================================================================================================");
-                     System.out.println("Enter any amount to deposit: ");
-                     System.out.println("=========================================================================================================");
-                     int amount = input.nextInt();
-                     deposit(amount);
-                     System.out.println("\n");
+            switch(inside) {
+
+                case 'A':
+                        System.out.print("Please insert a number: ");
+                       int amount = input.nextInt();
+                      MethodAddition(amount);
+                      System.out.println("The amount is : " + previousAmountAdd);
+                    break;
+
+                case 'B':
+                    System.out.print("Please insert a number: ");
+                    int amount1 = input.nextInt();
+                     MethodSubtraction(amount1);
+                     System.out.println("The amount is : " + previousAmountAdd);
                      break;
 
-                 case ('C'):
-                     System.out.println("=========================================================================================================");
-                     System.out.println("Enter an amount to withdraw");
-                     System.out.println("=========================================================================================================");
-                     int amount2 = input.nextInt();
-                     withdraw(amount2);
-                     System.out.println("\n");
-                     break;
+                case 'C':
+                   System.out.print("Please insert a number: ");
+                   int amount2 = input.nextInt();
+                   MethodMultiplication(amount2);
+                   System.out.println("The amount is : " + previousAmountAdd);
+                   break;
 
-                 case ('D'):
-                     System.out.println("=========================================================================================================");
-                     getPreviousTransaction();
-                     System.out.println("=========================================================================================================");
-                     System.out.println("\n");
-                     break;
+                case 'D':
+                    System.out.print("Please insert a number: ");
+                    int amount3 = input.nextInt();
+                    MethodDivision(amount3);
+                    System.out.println("The amount is : " + previousAmountAdd);
+                    break;
 
-                 case ('E'):
-                     System.out.println("*********************************************************************************************************");
-                                  break;
-                 default:
-                     System.out.println("Invalid option! please enter a new command again.");
-                     break;
+                case 'F':
+                    MethodErase(0);
+                    System.out.println("The amount is : " + previousAmountAdd);
+                    break;
+
+                case 'E':
+                    System.out.println("Thank you for using CalTextulator!");
+                    break;
+
+                default :
+                    System.out.println("Please choose within the options only.");
+
+                }
 
 
-             }
-        } while (option != 'E');
-        System.out.println("Thank you for using our service!");
+
+
+        } while (inside != 'E');
+
+
+
     }
 }
-
-
-
-
-
-
